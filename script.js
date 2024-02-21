da093a4e57f83ec31801dd50c55a87931a28065e
bri-depinto <144465570+bri-depinto@users.noreply.github.com>
2024-02-21 15:22:48 +0000

    java

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 let userName =prompt('Enter your name')
+let names='${userName} score'
+let userNameOutputDiv = document.getElementbyId('userNameOutput')
+console.log(names)
+userNameOutputDiv.innerHTML=`<p>${names}</p>`
