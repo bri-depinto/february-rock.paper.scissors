51f13101fbdf1f258c8ac96ceb53130c67511df5
bri-depinto <briannadepinto@gmail.com>
2024-02-27 09:10:32 -0500

    some java idk

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ while (playerName === null || playerName === '')[
     playerName = prompt('Please enter valid username:')
 ]
 document.getElementById('userName').innerText = `${playerName}'s`
+//getting the username and storing it
 
 let playerScore=0
 let computerScore=0
@@ -22,6 +23,7 @@ function playGame(playerChoice) {
     if(computerChoice === 'scissors'){
         document.getElementById('changeImage').src = 'images/sciz.png'
     }
+    //changing imahges based on what computer chose
 
     let result;
 
@@ -38,13 +40,24 @@ function playGame(playerChoice) {
         result = "Computer wins!";
         computerScore++;
         roundNum++;
-    }
 
+        if (roundNum===6){
+         const rButton = document.getElementById("rockButton");
+         rButton.disabled = true;
+         const pButton = document.getElementById("paperButton");
+         pButton.disabled = true;
+         const sButton = document.getElementById("scissorButton");
+         sButton.disabled = true;
+        }
+      
+    }
+    //updating round numbes and scores based on winner loser or tie
 
     document.getElementById('result').innerText = `You chose ${playerChoice}, computer chose ${computerChoice}. ${result}`;
     document.getElementById('computerScore').innerText = `${computerScore}`;
     document.getElementById('playerScore').innerText = `${playerScore}`;
     document.getElementById('roundNum').innerText = `${roundNum}`;
+    //showing the changes on the website
 }
 
 function resetGame() {
@@ -53,5 +66,8 @@ function resetGame() {
     roundNum = 1;
     document.getElementById('changeImage').src = 'images/question.png';
     document.getElementById('result').innerText = 'Who will win?';
-    document.getElementById('score').innerText = '';
+    document.getElementById('playerScore').innerText = '0';
+    document.getElementById('computerScore').innerText = '0';
+    document.getElementById('roundNum').innerText = '1';
 }
+//resetting the score, round, and displays
