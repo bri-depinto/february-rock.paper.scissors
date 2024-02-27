3caf31c44303e9a39889a55a4a9f11cf79716ee8
bri-depinto <144465570+bri-depinto@users.noreply.github.com>
2024-02-27 01:30:33 +0000

    MORE JAVA

--- script.js
+++ script.js
@@ -13,6 +13,16 @@ function playGame(playerChoice) {
     const choices = ['rock', 'paper', 'scissors'];
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
+    if(computerChoice === 'rock'){
+        document.getElementById('changeImage').src = 'images/rock.png'
+    }
+    if(computerChoice === 'paper'){
+        document.getElementById('changeImage').src = 'images/paper.png'
+    }
+    if(computerChoice === 'scissors'){
+        document.getElementById('changeImage').src = 'images/sciz.png'
+    }
+
     let result;
 
     if (playerChoice === computerChoice) {
@@ -25,13 +35,16 @@ function playGame(playerChoice) {
         result = "Computer wins!";
     }
 
+
     document.getElementById('result').innerText = `You chose ${playerChoice}, computer chose ${computerChoice}. ${result}`;
+
 }
 
 function resetGame() {
     playerScore = 0;
     computerScore = 0;
     roundNum = 1;
-    document.getElementById('result').innerText = '';
+    document.getElementById('changeImage').src = 'images/question.png';
+    document.getElementById('result').innerText = 'Who will win?';
     document.getElementById('score').innerText = '';
 }
